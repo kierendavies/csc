0f10d3b27da803958229088faf1d3bfe0bdf979f
Kieren Davies <kieren@kierendavies.co.za>
2012-08-13 02:46:35 +0200

    imageviewer phase one finished

--- android/imageviewer/gen/za/co/kierendavies/imageviewer/R.java
+++ android/imageviewer/gen/za/co/kierendavies/imageviewer/R.java
@@ -10,35 +10,32 @@ package za.co.kierendavies.imageviewer;
 public final class R {
     public static final class attr {
     }
-    public static final class color {
-        public static final int black=0x7f040007;
-        public static final int blue=0x7f040004;
-        public static final int cyan=0x7f040003;
-        public static final int green=0x7f040002;
-        public static final int magenta=0x7f040005;
-        public static final int red=0x7f040000;
-        public static final int white=0x7f040006;
-        public static final int yellow=0x7f040001;
-    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int aj=0x7f020000;
+        public static final int fs=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int pp=0x7f020003;
+        public static final int r=0x7f020004;
+        public static final int rd=0x7f020005;
+        public static final int ts=0x7f020006;
     }
     public static final class id {
-        public static final int button_next=0x7f060004;
-        public static final int button_pause=0x7f060003;
-        public static final int button_play=0x7f060002;
-        public static final int button_prev=0x7f060001;
-        public static final int image=0x7f060000;
+        public static final int banner=0x7f050000;
+        public static final int button_next=0x7f050004;
+        public static final int button_play_pause=0x7f050003;
+        public static final int button_prev=0x7f050002;
+        public static final int image=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int button_next=0x7f050005;
-        public static final int button_pause=0x7f050003;
-        public static final int button_play=0x7f050002;
-        public static final int button_prev=0x7f050004;
-        public static final int explanation=0x7f050001;
+        public static final int app_name=0x7f040000;
+        public static final int banner=0x7f040001;
+        public static final int button_next=0x7f040006;
+        public static final int button_pause=0x7f040004;
+        public static final int button_play=0x7f040003;
+        public static final int button_prev=0x7f040005;
+        public static final int image_description=0x7f040002;
     }
 }
--- android/imageviewer/src/za/co/kierendavies/imageviewer/ImageViewer.java
+++ android/imageviewer/src/za/co/kierendavies/imageviewer/ImageViewer.java
@@ -1,39 +1,82 @@
 package za.co.kierendavies.imageviewer;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ImageViewer extends Activity
-{
-    public static final String logtag = "ImageViewer";
-    Drawable[] images;
+public class ImageViewer extends Activity {
+    public static final int delay = 5000; // milliseconds
+    int[] imageIds = {
+            R.drawable.ts,
+            R.drawable.aj,
+            R.drawable.fs,
+            R.drawable.rd,
+            R.drawable.pp,
+            R.drawable.r,
+    };  // TODO maybe search for image files automatically
+    String[] titles = {
+            "Twilight Sparkle",
+            "Applejack",
+            "Fluttershy",
+            "Rainbow Dash",
+            "Pinkie Pie",
+            "Rarity"
+    };  // TODO generate titles from file names
+    int imageId = 0;
     ImageView imageView;
+    TextView banner;
+    boolean playing = false;
 
-    /** Called when the activity is first created. */
+    Handler handler = new Handler();
+    Runnable runnableNext = new Runnable() {
+        @Override
+        public void run() {
+            navNext(null);
+            handler.postDelayed(runnableNext, delay);
+        }
+    };
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Log.v(logtag, "blarg!");
-        this.imageView = (ImageView) findViewById(R.id.image);
-        this.images = new Drawable[]{};
+        imageView = (ImageView) findViewById(R.id.image);
+        imageView.setImageResource(imageIds[imageId]);
+        banner = (TextView) findViewById(R.id.banner);
+        banner.setText(titles[imageId]);
     }
 
     public void navPrev(View view) {
-        imageView.setImageResource(R.color.red);
+        imageId--;
+        if (imageId < 0) imageId += imageIds.length;  // java does modular arithmetic stupidly
+        imageView.setImageResource(imageIds[imageId]);
+        banner.setText(titles[imageId]);
     }
-    public void navPlay(View view) {
-        imageView.setImageResource(R.color.green);
+
+    public void navPlayPause(View view) {
+        Button button = (Button) view;
+        if (playing) {
+            //pause
+            playing = false;
+            handler.removeCallbacks(runnableNext);
+            button.setText(R.string.button_play);
+        } else {
+            //play
+            playing = true;
+            System.out.println("scheduling");
+            handler.postDelayed(runnableNext, 0);  // let's not delay the first time
+            System.out.println("modifying button");
+            button.setText(R.string.button_pause);
+        }
     }
-    public void navPause(View view) {
-        imageView.setImageResource(R.color.blue);
-    }
+
     public void navNext(View view) {
-        imageView.setImageResource(R.color.white);
+        imageId = (imageId + 1) % imageIds.length;
+        imageView.setImageResource(imageIds[imageId]);
+        banner.setText(titles[imageId]);
     }
 }
