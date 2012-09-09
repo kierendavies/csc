a5704c6412caf78bd5586551789e4b5e2231f094
Kieren Davies <kieren@kierendavies.co.za>
2012-09-09 14:05:53 +0200

    imageviewer improvements

--- android/imageviewer/gen/za/co/kierendavies/imageviewer/R.java
+++ android/imageviewer/gen/za/co/kierendavies/imageviewer/R.java
@@ -8,6 +8,10 @@
 package za.co.kierendavies.imageviewer;
 
 public final class R {
+    public static final class array {
+        public static final int imageIds=0x7f040000;
+        public static final int titles=0x7f040001;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -17,45 +21,49 @@ public final class R {
         public static final int av_pause=0x7f020003;
         public static final int av_play=0x7f020004;
         public static final int av_previous=0x7f020005;
-        public static final int content_picture=0x7f020006;
-        public static final int fs=0x7f020007;
-        public static final int ic_action_share=0x7f020008;
-        public static final int ic_launcher=0x7f020009;
-        public static final int images_rotate_left=0x7f02000a;
-        public static final int images_rotate_right=0x7f02000b;
-        public static final int pp=0x7f02000c;
-        public static final int r=0x7f02000d;
-        public static final int rd=0x7f02000e;
-        public static final int rename=0x7f02000f;
-        public static final int ts=0x7f020010;
+        public static final int collections_view_as_grid=0x7f020006;
+        public static final int content_picture=0x7f020007;
+        public static final int fs=0x7f020008;
+        public static final int ic_action_share=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int images_rotate_left=0x7f02000b;
+        public static final int images_rotate_right=0x7f02000c;
+        public static final int pp=0x7f02000d;
+        public static final int r=0x7f02000e;
+        public static final int rd=0x7f02000f;
+        public static final int rename=0x7f020010;
+        public static final int ts=0x7f020011;
     }
     public static final class id {
-        public static final int button_next=0x7f060003;
-        public static final int button_play_pause=0x7f060002;
-        public static final int button_prev=0x7f060001;
-        public static final int image=0x7f060000;
-        public static final int menu_full_screen=0x7f060004;
-        public static final int menu_rotate_left=0x7f060006;
-        public static final int menu_rotate_right=0x7f060007;
-        public static final int menu_share=0x7f060005;
+        public static final int button_next=0x7f070003;
+        public static final int button_play_pause=0x7f070002;
+        public static final int button_prev=0x7f070001;
+        public static final int image=0x7f070000;
+        public static final int menu_browser=0x7f070004;
+        public static final int menu_full_screen=0x7f070005;
+        public static final int menu_rotate_left=0x7f070006;
+        public static final int menu_rotate_right=0x7f070007;
+        public static final int menu_share=0x7f070008;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int browser=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int options=0x7f050000;
+        public static final int options=0x7f060000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int banner=0x7f040001;
-        public static final int button_next=0x7f040006;
-        public static final int button_pause=0x7f040004;
-        public static final int button_play=0x7f040003;
-        public static final int button_prev=0x7f040005;
-        public static final int image_description=0x7f040002;
-        public static final int menu_full_screen=0x7f04000a;
-        public static final int menu_rotate_left=0x7f040008;
-        public static final int menu_rotate_right=0x7f040009;
-        public static final int menu_share=0x7f040007;
+        public static final int app_name=0x7f050000;
+        public static final int banner=0x7f050001;
+        public static final int button_next=0x7f050006;
+        public static final int button_pause=0x7f050004;
+        public static final int button_play=0x7f050003;
+        public static final int button_prev=0x7f050005;
+        public static final int image_description=0x7f050002;
+        public static final int menu_browser=0x7f05000b;
+        public static final int menu_full_screen=0x7f05000a;
+        public static final int menu_rotate_left=0x7f050008;
+        public static final int menu_rotate_right=0x7f050009;
+        public static final int menu_share=0x7f050007;
     }
 }
