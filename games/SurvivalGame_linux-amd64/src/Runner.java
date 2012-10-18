5a5ae92e28cae5c23317e9f2899bfc354e1272b6
Kieren Davies <kieren@kierendavies.co.za>
2012-10-18 12:56:03 +0200

    set up textures, make collision detection not inverted, misc stuff

--- games/SurvivalGame_linux-amd64/src/Runner.java
+++ games/SurvivalGame_linux-amd64/src/Runner.java
@@ -5,6 +5,9 @@ import javax.swing.*;
 public class Runner {
     public static void main(String[] args) {
 
+        int screenWidth = 1280;
+        int screenHeight = 1024;
+
         JFrame frame = new JFrame("Survival Game");
         SurvivalGame ag = new SurvivalGame(100);
         ag.linkToFrame(frame);
@@ -13,7 +16,7 @@ public class Runner {
         frame.add(glc);
 
         frame.setResizable(false);
-        frame.setSize(800, 600);
+        frame.setSize(screenWidth, screenHeight);
         frame.setVisible(true);
 
         // need this so that you don't have to click on the window to gain focus ;)
--- games/SurvivalGame_linux-amd64/src/SurvivalGame.java
+++ games/SurvivalGame_linux-amd64/src/SurvivalGame.java
@@ -10,10 +10,6 @@ import java.awt.geom.Rectangle2D;
 import java.nio.ByteBuffer;
 import java.util.Vector;
 
-
-//==================================================================================================
-//==================================================================================================
-
 public class SurvivalGame extends Game {
     // Offset of the screen
     private Point2D.Float offset = new Point2D.Float(0, 0);
@@ -45,7 +41,7 @@ public class SurvivalGame extends Game {
     //GameFonts that will be used
     private GameFont arial, serif;
 
-    // The positin of the mouse
+    // The position of the mouse
     private Point2D.Float mousePos = new Point2D.Float(0, 0);
 
     // a counter for how far the mousewheel has been moved (just an example)
@@ -55,13 +51,9 @@ public class SurvivalGame extends Game {
     Point2D.Float[] linePositions = {new Point2D.Float(0, 0), new Point2D.Float(100, 100)};
     float[][] lineColours = {{1.0f, 1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f, 1.0f}};
 
-    //==================================================================================================
-
     public SurvivalGame(int GFPS) {
         super(GFPS);
     }
-
-    //==================================================================================================
 
     public void initStep(ResourceLoader loader) {
 
@@ -69,29 +61,58 @@ public class SurvivalGame extends Game {
         arial = loader.loadFont(new Font("Arial", Font.ITALIC, 48));
         serif = loader.loadFont(new Font("Serif", Font.PLAIN, 48));
 
-        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
 
         //Loading up our textures
         GameTexture softRockTexture = loader.loadTexture("Textures/soft_rock.png");
-        GameTexture rockTexture = loader.loadTexture("Textures/rock.png");
-        GameTexture grassTexture = loader.load("Textures/grass_tile.jpg");
+        GameTexture wallTexture = loader.loadTexture("Textures/rock.png");
+        GameTexture floorTexture = loader.loadTexture("Textures/sand_tile.png");
         bulletTexture = loader.loadTexture("Textures/bullet.png");
 
+        int gridSize = 10;
+        worldWidth = gridSize * floorTexture.getWidth();
+        worldHeight = gridSize * floorTexture.getHeight();
 
-        int gridSize = 12;
+        // creating the grid (with hacky sizing)
+        collisionGrid = new Grid(worldWidth+floorTexture.getWidth(), worldHeight+floorTexture.getWidth(), wallTexture.getWidth(), wallTexture.getHeight());
 
-        worldWidth = gridSize * grassTexture.getWidth();
-        worldHeight = gridSize * grassTexture.getHeight();
+        // creating the floor objects
+        gridTile = new GameObject[gridSize][gridSize];
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                gridTile[i][j] = new GameObject(floorTexture.getWidth() * i, floorTexture.getHeight() * j);
+                gridTile[i][j].addTexture(floorTexture, 0, 0);
+            }
+        }
 
-        collisionGrid = new Grid(worldWidth+64, worldHeight+64, rockTexture.getWidth(), rockTexture.getHeight());
-        // this sizing is hacked, I know
+        // Creating wall objects
+        for (int i = 0; i < floorTexture.getWidth() * gridSize; i += wallTexture.getWidth()) {
+            WallObject go = new WallObject(i, 0);
+            go.addTexture(wallTexture, 0, 0);
+            objects.add(go);
+            collisionGrid.add(go);
+
+            go = new WallObject(i, floorTexture.getHeight() * (gridSize - 1));
+            go.addTexture(wallTexture, 0, 0);
+            objects.add(go);
+            collisionGrid.add(go);
+        }
+        for (int i = floorTexture.getHeight(); i < floorTexture.getHeight() * (gridSize - 1); i += wallTexture.getHeight()) {
+            WallObject go = new WallObject(0, i);
+            go.addTexture(wallTexture, 0, 0);
+            objects.add(go);
+            collisionGrid.add(go);
+
+            go = new WallObject(wallTexture.getWidth() * (gridSize - 1), i);
+            go.addTexture(wallTexture, 0, 0);
+            objects.add(go);
+            collisionGrid.add(go);
+        }
 
         // creating some random rocks to shoot
         for (int i = 0; i < 8; i++) {
 
-            float x = (float) ((Math.random() * (gridSize - 4) + 2) * grassTexture.getWidth());
-            float y = (float) ((Math.random() * (gridSize - 4) + 2) * grassTexture.getHeight());
+            float x = (float) ((Math.random() * (gridSize - 4) + 2) * floorTexture.getWidth());
+            float y = (float) ((Math.random() * (gridSize - 4) + 2) * floorTexture.getHeight());
 
             GameObject go = new GameObject(x, y);
             go.addTexture(softRockTexture, 0, 0);
@@ -99,60 +120,19 @@ public class SurvivalGame extends Game {
             collisionGrid.add(go);
         }
 
-
-        // creating the floor objects
-        gridTile = new GameObject[gridSize][gridSize];
-        for (int i = 0; i < gridSize; i++) {
-            for (int j = 0; j < gridSize; j++) {
-                gridTile[i][j] = new GameObject(grassTexture.getWidth() * i, grassTexture.getHeight() * j);
-                gridTile[i][j].addTexture(grassTexture, 0, 0);
-            }
-        }
-
-
-        // Creating wall objects
-        for (int i = 0; i < grassTexture.getWidth() * gridSize; i += rockTexture.getWidth()) {
-            WallObject go = new WallObject(i, 0);
-            go.addTexture(rockTexture, 0, 0);
-            objects.add(go);
-            collisionGrid.add(go);
-
-            go = new WallObject(i, grassTexture.getHeight() * (gridSize - 1));
-            go.addTexture(rockTexture, 0, 0);
-            objects.add(go);
-            collisionGrid.add(go);
-        }
-        for (int i = grassTexture.getHeight(); i < grassTexture.getHeight() * (gridSize - 1); i += rockTexture.getHeight()) {
-            WallObject go = new WallObject(0, i);
-            go.addTexture(rockTexture, 0, 0);
-            objects.add(go);
-            collisionGrid.add(go);
-
-            go = new WallObject(rockTexture.getWidth() * (gridSize - 1), i);
-            go.addTexture(rockTexture, 0, 0);
-            objects.add(go);
-            collisionGrid.add(go);
-        }
-
-        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
         // Creating the player's ship
         player = new PlayerObject(
-                (float) (grassTexture.getWidth() * gridSize) / 2f,
-                (float) (grassTexture.getHeight() * gridSize) / 2f);
+                (float) (floorTexture.getWidth() * gridSize) / 2f,
+                (float) (floorTexture.getHeight() * gridSize) / 2f);
 
         for (int i = 0; i < 72; i++) {
-            player.addTexture(loader.load("Textures/ship/spaceship_sm" + i + ".gif"), 16, 16);
+            player.addTexture(loader.load("Textures/dude/dude" + i + ".png"), 64, 64);
         }
 
-        objects.add(player);
         // don't add player to grid - this is slightly faster because player is so dynamic
         // and it is safe because it is the only object not in the grid
-
-        // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+        objects.add(player);
     }
-
-    //==================================================================================================
 
     // this method is used to fire a bullet 
     public void fireBullet() {
@@ -162,8 +142,8 @@ public class SurvivalGame extends Game {
         float dir = 90 + player.getDegreesTo(mousePos);
         BulletObject bullet =
                 new BulletObject(
-                        player.getPosition().x + (float) Math.sin(Math.toRadians(dir)) * 32,
-                        player.getPosition().y - (float) Math.cos(Math.toRadians(dir)) * 32,
+                        player.getPosition().x + (float) Math.sin(Math.toRadians(dir - 5)) * 60,
+                        player.getPosition().y - (float) Math.cos(Math.toRadians(dir - 5)) * 60,
                         1f, 300, bulletTexture, collisionGrid);
 
         //bullet.setVelocity(player.getVelocity());
@@ -220,33 +200,7 @@ public class SurvivalGame extends Game {
 
     }
 
-    //==================================================================================================
-
-    public void logicStep(GameInputInterface gii) {
-
-        // some examples of the mouse interface
-        mouseWheelTick += gii.mouseWheelRotation();
-        mousePos.x = (float) gii.mouseXScreenPosition() - offset.x;
-        mousePos.y = (float) gii.mouseYScreenPosition() - offset.y;
-
-        //----------------------------------
-
-        if (alive) {
-            handlePlayerMovement(gii);
-            player.setDirection(90 + player.getDegreesTo(mousePos));
-        }
-
-        // NOTE: you must call doTimeStep for ALL game objects once per frame!
-        // updating step for each object
-        for (int i = 0; i < objects.size(); i++) {
-            objects.elementAt(i).doTimeStep();
-        }
-
-        // setting the camera offset
-        offset.x = -player.getPosition().x + (this.getViewportDimension().width / 2);
-        offset.y = -player.getPosition().y + (this.getViewportDimension().height / 2);
-
-
+    private void handleCollisions() {
         // destroy bullets outside the game area because they break collision detection
         for (int i = 0; i < objects.size(); i++) {
             if (objects.elementAt(i) instanceof BulletObject) {
@@ -285,8 +239,9 @@ public class SurvivalGame extends Game {
 
                 for (int x = left; x <= right; x++) {
                     for (int y = top; y <= bottom; y++) {
-                        int a1 = buffer1.get(((x-bb1.x) + (y-bb1.y)*bb1.width)*4 + 3);
-                        int a2 = buffer2.get(((x-bb2.x) + (y-bb2.y)*bb2.width)*4 + 3);
+                        // turns out i was doing this upside down last time
+                        int a1 = buffer1.get(((x-bb1.x) + (bb1.height-y+bb1.y-1)*bb1.width)*4 + 3);
+                        int a2 = buffer2.get(((x-bb2.x) + (bb2.height-y+bb2.y-1)*bb2.width)*4 + 3);
 
                         if (a1 == -1 && a2 == -1) {  // full alpha on both pixels
                             collides = true;
@@ -339,6 +294,32 @@ public class SurvivalGame extends Game {
 
     //==================================================================================================
 
+    public void logicStep(GameInputInterface gii) {
+
+        // some examples of the mouse interface
+        mouseWheelTick += gii.mouseWheelRotation();
+        mousePos.x = (float) gii.mouseXScreenPosition() - offset.x;
+        mousePos.y = (float) gii.mouseYScreenPosition() - offset.y;
+
+        //----------------------------------
+
+        if (alive) {
+            handlePlayerMovement(gii);
+            player.setDirection(90 + player.getDegreesTo(mousePos));
+        }
+
+        // NOTE: you must call doTimeStep for ALL game objects once per frame!
+        // updating step for each object
+        for (int i = 0; i < objects.size(); i++) {
+            objects.elementAt(i).doTimeStep();
+        }
+
+        // setting the camera offset
+        offset.x = -player.getPosition().x + (this.getViewportDimension().width / 2);
+        offset.y = -player.getPosition().y + (this.getViewportDimension().height / 2);
+
+        handleCollisions();
+    }
 
     public void renderStep(GameDrawer drawer) {
         //For every object that you want to be rendered, you must call the draw function with it as a parameter
@@ -381,7 +362,8 @@ public class SurvivalGame extends Game {
 
         // Some debug type info to demonstrate the font drawing
         if (player != null) {
-            drawer.draw(arial, "" + player.getDirection(), new Point2D.Float(20, 120), 1.0f, 0.5f, 0.0f, 0.7f, 0.1f);
+            //drawer.draw(arial, "" + player.getDirection(), new Point2D.Float(20, 120), 1.0f, 0.5f, 0.0f, 0.7f, 0.1f);
+            drawer.draw(arial, "" + player.getActiveTexture(), new Point2D.Float(20, 120), 1.0f, 0.5f, 0.0f, 0.7f, 0.1f);
         }
         drawer.draw(arial, "" + mouseWheelTick, new Point2D.Float(20, 68), 1.0f, 0.5f, 0.0f, 0.7f, 0.1f);
         drawer.draw(serif, "" + mousePos.x + ":" + mousePos.y, new Point2D.Float(20, 20), 1.0f, 0.5f, 0.0f, 0.7f, 0.1f);
