dde6146bee053e2627d67262d59a0d9880785587
Kieren Davies <kieren@kierendavies.co.za>
2013-09-14 15:48:49 +0300

    redo golseq with proper grid resizing

--- parallel2013/GameOfLifeSequential.java
+++ parallel2013/GameOfLifeSequential.java
@@ -6,89 +6,202 @@ import com.googlecode.lanterna.terminal.text.UnixTerminal;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.Scanner;
 
 public class GameOfLifeSequential {
-    int width, height;
-    boolean[][] grid;
+    static final double resizeFactor = 1.5;
+    int oldLeft, oldTop, oldWidth, oldHeight;
+    int newLeft, newTop, newWidth, newHeight;
+    boolean[][] oldGrid;
+    boolean[][] newGrid;
     int population;
 
-    public GameOfLifeSequential(int height, int width) {
-        this.height = height;
-        this.width = width;
-        this.grid = new boolean[height][width];
-        this.population = 0;
+    public GameOfLifeSequential(int width, int height) {
+        oldWidth = width;
+        oldHeight = height;
+        oldLeft = 0;
+        oldTop = 0;
+        newWidth = width;
+        newHeight = height;
+        newLeft = 0;
+        newTop = 0;
+        oldGrid = new boolean[oldWidth][oldHeight];
+        newGrid = new boolean[newWidth][newHeight];
+        population = 0;
+    }
+
+    private boolean getCell(int x, int y) {
+        return getOldCell(x, y);
+    }
+
+    private boolean getOldCell(int x, int y) {
+        if (x < oldLeft || x >= oldLeft +  oldWidth ||
+                y < oldTop || y >= oldTop + oldHeight) {
+            return false;
+        }
+        return oldGrid[x - oldLeft][y - oldTop];
+    }
+
+    private void setOldCellLive(int x, int y) {
+        oldGrid[x - oldLeft][y - oldTop] = true;
+    }
+
+    private void setNewCellLive(int x, int y) {
+        newGrid[x - newLeft][y - newTop] = true;
     }
 
     public void randomise() {
         population = 0;
         Random random = new Random();
-        for (int r = 0; r < height; ++r) {
-            for (int c = 0; c < width; ++c) {
-                grid[r][c] = random.nextBoolean();
-                if (grid[r][c]) {
+        for (int x = 1; x < oldWidth - 1; ++x) {
+            for (int y = 1; y < oldHeight - 1; ++y) {
+                oldGrid[x][y] = random.nextBoolean();
+                if (oldGrid[x][y]) {
                     ++population;
                 }
             }
         }
+        oldLeft = -oldWidth/2;
+        oldTop = -oldHeight/2;
+        newLeft = oldLeft;
+        newTop = oldTop;
     }
 
-    public void load(String name) throws FileNotFoundException, ArrayIndexOutOfBoundsException {
-        grid = new boolean[height][width];
+    public void load(String name) throws IOException {
         population = 0;
         if (!name.startsWith("patterns/")) name = "patterns/" + name;
         if (!name.endsWith(".cells")) name = name + ".cells";
-        Scanner scanner = new Scanner(new File(name));
-        int r = 0;
-        while (scanner.hasNext()) {
-            String line = scanner.nextLine();
+        BufferedReader reader = new BufferedReader(new FileReader(name));
+        String line;
+
+        // skip comments
+        while (reader.readLine().startsWith("!")) {
+            // mark beginning, gets overwritten until it is correct
+            reader.mark(200000);  // should be an adequate readahead limit
+        }
+
+        // check pattern size
+        int width = 0, height = 0;
+        while ((line = reader.readLine()) != null) {
+            ++height;
+            if (line.length() > width) width = line.length();
+        }
+        width += 2;  // necessary padding
+        height += 2;
+        if (width > oldWidth || height > oldHeight) {
+            if (width > oldWidth) {
+                oldWidth = width;
+                newWidth = width;
+            }
+            if (height > oldHeight) {
+                oldHeight = height;
+                newHeight = height;
+            }
+            oldGrid = new boolean[oldWidth][oldHeight];
+            newGrid = new boolean[newWidth][newHeight];
+        }
+
+        // assign cells
+        reader.reset();
+        int y = 1;
+        while ((line = reader.readLine()) != null) {
             if (line.startsWith("!")) continue;
-            for (int c = 0; c < line.length(); ++c) {
-                if (line.charAt(c) != ' ' && line.charAt(c) != '\n' && line.charAt(c) != '.') {
-                    grid[r][c] = true;
+            for (int x = 0; x < line.length(); ++x) {
+                if (line.charAt(x) != ' ' && line.charAt(x) != '\n' && line.charAt(x) != '.') {
+                    oldGrid[x + 1][y] = true;
                     ++population;
                 }
             }
-            ++r;
+            ++y;
         }
+
+        oldLeft = -oldWidth/2;
+        oldTop = -oldHeight/2;
+        newLeft = oldLeft;
+        newTop = oldTop;
     }
 
     public void update() {
-        boolean[][] newGrid = new boolean[height][width];
         int population = 0;
-        for (int r = 0; r < height; ++r) {
-            for (int c = 0; c < width; ++c) {
+        boolean resizeLeft = false,
+                resizeRight = false,
+                resizeUp = false,
+                resizeDown = false;
+
+        // iterate over all cells
+        for (int x = newLeft; x < newLeft + newWidth; ++x) {
+            for (int y = newTop; y < newTop + newHeight; ++y) {
                 int neighbours = 0;
-                for (int dr = -1; dr <= 1; ++dr) {
-                    for (int dc = -1; dc <= 1; ++dc) {
-                        if (dr == 0 && dc == 0) continue;
-                        if (grid[(r + dr + height) % height][(c + dc + width) % width]) {
+                for (int dx = -1; dx <= 1; ++dx) {
+                    for (int dy = -1; dy <= 1; ++dy) {
+                        if (dx == 0 && dy == 0) continue;
+                        if (getOldCell(x + dx, y + dy)) {
                             ++neighbours;
                         }
                     }
                 }
-                if (neighbours == 3 || (grid[r][c] && neighbours == 2)) {
-                    newGrid[r][c] = true;
+                if (neighbours == 3 || (getOldCell(x, y) && neighbours == 2)) {
+                    setNewCellLive(x, y);
                     ++population;
+                    // check if grid needs resizing
+                    if (x == newLeft) resizeLeft = true;
+                    if (x == newLeft + newWidth - 1) resizeRight = true;
+                    if (y == newTop) resizeUp = true;
+                    if (y == newTop + newHeight - 1) resizeDown = true;
                 }
             }
         }
-        grid = newGrid;
+
         this.population = population;
+
+        // switch grids
+        boolean[][] tmp = oldGrid;
+        oldGrid = newGrid;
+        if (resizeLeft || resizeRight || resizeUp || resizeDown) {
+            // reallocate new grid
+            // scale from midpoint in designated direction
+            if (resizeLeft) {
+                newLeft -= (int) Math.ceil(resizeFactor * oldWidth / 2);
+                newWidth += (int) Math.ceil(resizeFactor * oldWidth / 2);
+            }
+            if (resizeRight) {
+                newWidth += (int) Math.ceil(resizeFactor * oldWidth / 2);
+            }
+            if (resizeUp) {
+                newTop -= (int) Math.ceil(resizeFactor * oldHeight / 2);
+                newHeight += (int) Math.ceil(resizeFactor * oldHeight / 2);
+            }
+            if (resizeDown) {
+                newHeight += (int) Math.ceil(resizeFactor * oldHeight / 2);
+            }
+            newGrid = new boolean[newWidth][newHeight];
+        } else if (oldWidth != newWidth || oldHeight != newHeight) {
+            oldLeft = newLeft;
+            oldTop = newTop;
+            oldWidth = newWidth;
+            oldHeight = newHeight;
+            newGrid = new boolean[newWidth][newHeight];
+        } else {
+            // reuse old grid
+            newGrid = tmp;
+            for (int x = 0; x < newWidth; ++x) {
+                for (int y = 0; y < newHeight; ++y) {
+                    newGrid[x][y] = false;
+                }
+            }
+        }
     }
 
-    public void draw(Screen screen, int rows, int cols) {
-        if (rows > height) rows = height;
-        if (cols > width) cols = width;
-        for (int r = 0; r < rows; ++r) {
-            for (int c = 0; c < cols; ++c) {
-                if (grid[r][c]) {
+    public void draw(Screen screen, int left, int top, int width, int height) {
+        for (int r = 0; r < height; ++r) {
+            for (int c = 0; c < width; ++c) {
+                if (getCell(c + left, r + top)) {
                     screen.putString(c, r+1, "#", Color.WHITE, Color.WHITE);
                 } else {
                     screen.putString(c, r+1, " ", Color.BLACK, Color.BLACK);
@@ -97,10 +210,14 @@ public class GameOfLifeSequential {
         }
     }
 
+    public void draw(Screen screen, int width, int height) {
+        draw(screen, -width/2, -height/2, width, height);
+    }
+
     public void draw(Screen screen) {
         draw(screen,
-             screen.getTerminal().getTerminalSize().getRows() - 1,
-             screen.getTerminal().getTerminalSize().getColumns());
+             screen.getTerminal().getTerminalSize().getColumns(),
+             screen.getTerminal().getTerminalSize().getRows() - 1);
     }
 
     public static void main(String[] args) {
@@ -108,7 +225,7 @@ public class GameOfLifeSequential {
         parser.acceptsAll(
                 Arrays.asList("n", "no-draw"),
                 "Do not draw the grid, so that performance can be benchmarked.  " +
-                        "Requires width, height, and generations to be set.");
+                        "Requires generations to be set.");
         parser.acceptsAll(
                 Arrays.asList("g", "generations"),
                 "Number of generations to run before halting.  If set to 0 it will run indefinitely.")
@@ -118,18 +235,16 @@ public class GameOfLifeSequential {
                 .defaultsTo(0);
         parser.acceptsAll(
                 Arrays.asList("w", "width"),
-                "Set the width of the entire cell grid.  If set to 0 the width of the terminal is used.")
-                .requiredIf("no-draw")
+                "Set the initial width of the cell grid.")
                 .withRequiredArg()
                 .ofType(Integer.class)
-                .defaultsTo(0);
+                .defaultsTo(100);
         parser.acceptsAll(
                 Arrays.asList("h", "height"),
-                "Set the height of the entire cell grid.  If set to 0 the height of the terminal is used.")
-                .requiredIf("no-draw")
+                "Set the initial height of the cell grid.")
                 .withRequiredArg()
                 .ofType(Integer.class)
-                .defaultsTo(0);
+                .defaultsTo(100);
         parser.acceptsAll(
                 Arrays.asList("p", "pattern"),
                 "Load a specified pattern.")
@@ -197,11 +312,8 @@ public class GameOfLifeSequential {
         if (options.hasArgument("pattern") && !options.has("random")) {
             try {
                 gol.load((String) options.valueOf("pattern"));
-            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
                 System.out.println("pattern not found");
-                System.exit(2);
-            } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println("pattern too large for grid");
                 System.exit(2);
             }
         } else {
